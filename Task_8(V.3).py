0a1001c9ed8c31ecd60871e251d5b283c164188c
Egor <123231130+Darkness-Warrior@users.noreply.github.com>
2023-06-17 00:56:50 +0300

    Update Task_8(V.3).py

--- Task_8(V.3).py
+++ Task_8(V.3).py
@@ -1,36 +1,23 @@
-import time
+import datetime
 
 
-def calculate_taxi_payment(distance):
-    meters = distance * 1000
-    base_fee = 4.00
-    additional_fee = (meters // 140) * 0.25
-    total_fee = base_fee + additional_fee
-    return total_fee
-
-
-def log_execution_time(func):
+def log(func):
     def wrapper(distance):
-        print(f"Function '{func.__name__}' was called at {time.strftime('%Y-%m-%d %H:%M:%S')}")
-        print(f"Input parameters: {distance} km")
+        print(f"Function {func.__name__} started at {datetime.datetime.now()}")
+        print(f"Input distance: {distance}")
         result = func(distance)
-        print(f"Result: {result} USD")
+        print(f"Output result: {result}")
         return result
     return wrapper
 
 
-@log_execution_time
-def calculate_taxi_payment_with_log(distance):
-    meters = distance * 1000
-    base_fee = 4.00
-    additional_fee = (meters // 140) * 0.25
-    total_fee = base_fee + additional_fee
-    return total_fee
+@log
+def total_taxi_fare(distance):
+    base = 4.0
+    total_fare = base + (distance * 1000 / 140) * 0.25  # Рассчитываем итоговою стоимость
+    return round(total_fare, 2)
 
 
-if __name__ == "__main__":
-    distance = 10
-    print(f"The taxi payment for {distance} km is {calculate_taxi_payment(distance)} USD")
-    print()
-    calculate_taxi_payment_with_log(distance)
+distance = 9.6 #Километров
+total_taxi_fare(distance)
 
